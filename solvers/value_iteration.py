5732534c8214fd5b63153e1f923ded4e8a4ad7e4
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2025-09-23 23:59:30 -0700

    Update value_iteration.py

--- solvers/value_iteration.py
+++ solvers/value_iteration.py
@@ -68,3 +68,31 @@ def value_iteration_deterministic(grid_world,
   return Vk, P
 
 
+def policy_evaluation(grid_world: GridWorld, 
+                      policy: np.array,
+                      gamma: float=0.9,
+                      theta: float=1e-10,
+                      max_iter: int=500):
+  Vk = np.zeros((grid_world.height, grid_world.width)) #Old Value Matrix
+  for i in range(grid_world.height):
+    for j in range(grid_world.width):
+        if (i, j) in grid_world.terminal:
+          Vk[i, j] = grid_world.rewards[i, j]
+
+  Vk1 = Vk.copy() #New Value Matrix
+  for iter in range(max_iter):
+    for i in range(grid_world.height):
+      for j in range(grid_world.width):
+        if (i, j) not in grid_world.terminal:
+          possible_outcomes = grid_world.get_transition_probs((i, j), grid_world.actions[policy[i, j]])
+          q_val = 0.0
+          for prob, new_state, reward in possible_outcomes:
+            q_val += prob * (reward + gamma * Vk[new_state])
+          Vk1[i, j] = q_val
+    max_diff = np.max(np.abs(Vk - Vk1))
+    Vk = Vk1.copy()
+    if max_diff < theta:
+      print(f"Policy Evaluation converged at sub-iteration {iter} with Theta: {max_diff}")
+      break
+  return Vk
+
