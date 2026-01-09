5c39c2b1578755beebc2b76befb0096042b1cb4f
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2026-01-08 23:48:15 -0800

    Update dqn.py

--- solvers/dqn/dqn.py
+++ solvers/dqn/dqn.py
@@ -2,5 +2,14 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.optim import Adam
+from torch.utils.data import Dataset, DataLoader
+
+
+class Network(nn.Module):
+  def __init__(self):
+    super().__init__()
+
+  def forward(self, x):
+    return x
 
 
